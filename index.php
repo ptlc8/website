fd36e796f0ea42a1d1c7730cc81d1571376a5b11
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-07-08 17:30:33 +0200

    update metadata

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=0.5" />
 		<link rel="shortcut icon" href="favicon.ico" />
 		<meta name="language" content="fr-FR" />
-		<meta name="sitename" content="agnd.fr" />
-		<meta name="keywords" content="agenda,partagé,partage,poké,profs,poképrofs,calculateur,moyenne,eisti,cy,tech,agendas,share,shared,teachers,cards,cartes,prof,agnd,jmas,jmax,abo,abonosaure,smash,meme,histoire" />
+		<meta name="sitename" content="ambi.dev" />
+		<meta name="keywords" content="agenda,partagé,partage,poké,profs,poképrofs,calculateur,moyenne,eisti,cy,tech,agendas,share,shared,teachers,cards,cartes,prof,agnd,jmas,jmax,abo,abonosaure,smash,meme,histoire,cy-hub,hub" />
 		<meta name="description" content="Sur ce site, vous trouverez des agendas partageables, le jeu de carte à jouer Poképrofs et un calculateur de moyenne." />
 		<meta name="robots" content="index, follow" />
-		<meta name="copyright" content="PTLC - Tous droits réservés" />
-		<meta name="author" content="PTLC" />
+		<meta name="copyright" content="Ambi / PTLC - Tous droits réservés" />
+		<meta name="author" content="Ambi alias PTLC" />
+		<link rel="canonical" href="https://ambi.dev/" />
 		<script src="utils.js" async></script>
 	</head>
 	<body>
