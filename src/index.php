b5a8c70ca38fd020442832bf5b42005535364cb8
Ambi <ptlc.ptlc.ptlc@gmail.com>
2026-02-21 17:05:39 -0500

    Fix some HTML

--- src/index.php
+++ src/index.php
@@ -4,10 +4,9 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title><?= htmlspecialchars(get_site_name()) ?> - Projets</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="style.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="shortcut icon" href="favicon.ico" />
+		<link rel="icon" href="favicon.ico" />
 		<meta name="language" content="fr" />
 		<meta name="sitename" content="<?= htmlspecialchars(get_site_name()) ?>" />
 		<meta name="keywords" content="<?= htmlspecialchars(get_site_data()->keywords) ?>" />
